c93747301c434472182d1ab1dd600808f2117008
Isaac Kaufman <ikaufman@gatech.edu>
2017-03-08 12:41:24 -0500

    New behavior on click when imdb not current tab

--- addMovie.js
+++ addMovie.js
@@ -9,38 +9,52 @@ function checkUrl(tabs) {
         var movieId = url.substring(26, 35);
         loadMovie(movieId);
     } else {
-        alert("No movie found, go to imdb");
+        // just fill the watchlist, no current movie
+        loadMovie(-1);
     }
 
 }
 
 /** loads current movie */
 function loadMovie(id) {
-    var xhr = new XMLHttpRequest();
-    var request = "https://api.themoviedb.org/3/movie/".concat(id, "?api_key=18da41385acb8a93f0771cebf418f8a9");
+    
+    if (id != -1) {
+        var xhr = new XMLHttpRequest();
+        var request = "https://api.themoviedb.org/3/movie/".concat(id, "?api_key=18da41385acb8a93f0771cebf418f8a9");
+        
+        xhr.open('GET', request,  true);
+        xhr.send();
+        xhr.addEventListener("readystatechange", processRequest, false);
 
-    xhr.open('GET', request,  true);
-    xhr.send();
-    xhr.addEventListener("readystatechange", processRequest, false);
+        xhr.onreadystatechange = processRequest;
 
-    xhr.onreadystatechange = processRequest;
-
-    var isDone = false;
-    function processRequest(e) {
-        if (xhr.readyState == 4 && xhr.status == 200 && isDone == false) {
-            var response = JSON.parse(xhr.responseText);
-            setCurrentMovie(response.title, response.release_date, response.poster_path);
-            updateMovieList();
-            isDone = true;
+        var isDone = false;
+        function processRequest(e) {
+            if (xhr.readyState == 4 && xhr.status == 200 && isDone == false) {
+                var response = JSON.parse(xhr.responseText);
+                setCurrentMovie(response.title, response.release_date, response.poster_path);
+                updateMovieList();
+                isDone = true;
+            }
         }
     }
-
+    else {
+        displayData(movies, -1);
+    }
 }
 
 
 /** displays watchlist and current movie to popup.html */
 function displayData(movies, currentMovie) {
     
+    clearData();
+    
+    if (currentMovie != -1) { populateCurrentMovie(currentMovie); }
+    else { showLogo(); }
+    populateWatchlist(movies);
+}
+
+function clearData() {
     // clear out the current watchlist and movie data
     // should be temporary until we decide on a framework
     var myNode = document.getElementById("watchlist");
@@ -52,14 +66,9 @@ function displayData(movies, currentMovie) {
     while (myNode.firstChild) {
         myNode.removeChild(myNode.firstChild);
     }
-    
-    var poster = document.createElement("IMG");
-    poster.setAttribute("src", "https://image.tmdb.org/t/p/w500".concat(currentMovie.imgId));
-    poster.setAttribute("width", "91");
-    poster.setAttribute("height", "143");
+}
 
-    document.getElementById("currentMovie").appendChild(poster);
-    html = "";
+function populateWatchlist(movies) {
     for (i = 0; i < movies.length; i++) {
         // chop off the month and day of the release date
         // the year field is now a misnomer, consider renaming to release date
@@ -108,6 +117,31 @@ function displayData(movies, currentMovie) {
         text.innerText = movies[i].title + " - " + movies[i].year.substring(0,4);
         document.getElementById(innerATextId).appendChild(text);
     }
+}
+
+function populateCurrentMovie (currentMovie) {
+    var poster = document.createElement("IMG");
+    poster.setAttribute("src", "https://image.tmdb.org/t/p/w500".concat(currentMovie.imgId));
+    poster.setAttribute("width", "91");
+    poster.setAttribute("height", "143");
+
+    document.getElementById("currentMovie").appendChild(poster);
+}
+
+function showLogo() {
+    document.getElementById("addButton").style.display = "none";
+    document.getElementById("header2").innerHTML = "IMDb";
+    
+    var link = document.createElement("A");
+    link.setAttribute("href", "http://www.imdb.com");
+    link.setAttribute("id", "logoLink");
+    link.setAttribute("target", "_blank");
+    var logo = document.createElement("IMG");
+    logo.setAttribute("src", "http://ia.media-imdb.com/images/M/MV5BMTczNjM0NDY0Ml5BMl5BcG5nXkFtZTgwMTk1MzQ2OTE@._V1_.png");
+    logo.setAttribute("width", "100px");
+    logo.setAttribute("margin-left", "50px");
+    document.getElementById("currentMovie").appendChild(link);
+    document.getElementById("logoLink").appendChild(logo);
 }
 
 window.onload = function onLoad() {
